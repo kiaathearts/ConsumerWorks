d68cd0492fd987b919b340bce7d510c0c835ee55
Goode <n00121210@ospreys.unf.edu>
2019-04-15 20:10:26 -0400

    Add project files.

---
+++ ConsumerWorks/Controllers/ConsumerWorksController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumerWorks.Controllers
+{
+    public class ConsumerWorksController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Help()
+        {
+            return View();
+        }
+
+        public ActionResult Contracts() {
+            return View();
+        }
+
+        public ActionResult About() {
+            return View();
+        }
+
+        public ActionResult PersonalProfile() {
+            return View();
+        }
+
+    }
+
+}
